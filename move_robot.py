c93b63fad11ae5aa365eee26c9bebda2a841dedb
murilobicho@gmail.com <murilobicho@gmail.com>
2023-04-24 21:01:53 -0300

    First Commit

--- move_robot.py
+++ move_robot.py
@@ -2,4 +2,4 @@ from Communication import Communication # Aqui importamos a classe Communication
 
 info = Communication()  # Logo depois declaramos uma variavel que serve para receber e enviar as informações recebidas
 
-info.move(0, True, 11, 10)
+info.move(0, True, 10, 10)
