fa3491e56416c9d4c73a2238a527cf655a180092
murilobicho@gmail.com <murilobicho@gmail.com>
2023-04-24 21:19:05 -0300

    First Commit

--- ball_position.py
+++
@@ -1,9 +0,0 @@
-from Communication import Communication # Aqui importamos a classe Communication
-
-info = Communication()  # Logo depois declaramos uma variável que serve para receber e enviar as informações recebidas
-
-ball = info.ball()  # em seguida declaramos uma variável para receber a posição da bola
-
-ball_x = ball.x
-ball_y = ball.y
-print(ball_x, ball_y)  # e printamos a posição recebida
--- move_robot.py
+++
@@ -1,5 +0,0 @@
-from Communication import Communication # Aqui importamos a classe Communication
-
-info = Communication()  # Logo depois declaramos uma variavel que serve para receber e enviar as informações recebidas
-
-info.move(0, True, 10, 10)
